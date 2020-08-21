305db17762c2b35e2f61e6dfc8936a92ff773603
ricardofisma <ricardomallqui6@gmail.com>
2020-08-21 18:34:49 -0500

    w

--- server.js
+++ server.js
@@ -10,6 +10,13 @@ var userSchemaJSON = {
 var user_schema = new Schema(userSchemaJSON);
 var User = mongoose.model("User", user_schema);
 
+app.post("/users", function (req, res) {
+  var user = new User({ email: req.body.email, pass: req.body.pass });
+  user.save(function () {
+    res.send("Recibimos tus datos");
+  });
+});
+
 const http = require("http");
 
 const hostname = "127.0.0.1";
@@ -17,6 +24,7 @@ const port = 3000;
 
 app.use(express.static("public"));
 
-app.listen(3000, () => {
+const port = process.env.PORT || 5000;
+app.listen(port, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
 });
